644cb2520ea04e85c22ed0b6f248adcc09d87647
HM <hieumdd@gmail.com>
2025-03-06 14:29:30 +0700

    add: Add qdrant

--- db.py
+++ db.py
@@ -1,7 +1,9 @@
 import os
 
 import chromadb
+from qdrant_client import QdrantClient
 import redis
 
 CHROMA_CLIENT = chromadb.PersistentClient("./.chroma")
+QDRANT_CLIENT = QdrantClient(url=os.getenv("QDRANT_URL"))
 REDIS_CLIENT = redis.Redis.from_url(os.getenv("REDIS_URL"))
