00b2b57d2da41ff55eabd24e8dc9fe0683504382
HM <hieumdd@gmail.com>
2025-03-13 10:20:33 +0700

    add: Upgrade to ChatModel & ChatPrompt

--- rag.py
+++ rag.py
@@ -2,10 +2,19 @@ from datetime import datetime
 from functools import lru_cache
 import re
 
+from langchain.schema import AIMessage
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.documents import Document
-from langchain_core.prompts import PromptTemplate
-from langchain_google_genai import GoogleGenerativeAI, HarmCategory, HarmBlockThreshold
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+)
+from langchain_google_genai import (
+    ChatGoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+)
 
 from logger import get_logger
 from config import Config
@@ -17,7 +26,8 @@ logger = get_logger(__name__)
 @lru_cache(1)
 def llm(config=Config):
     logger.debug("Initialized LLM")
-    return GoogleGenerativeAI(
+    return ChatGoogleGenerativeAI(
+        temperature=1.0,
         model="gemini-2.0-flash",
         safety_settings={
             HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
@@ -33,7 +43,7 @@ def llm(config=Config):
 @lru_cache(1)
 def prompt():
     logger.debug("Initialized Prompt")
-    return PromptTemplate.from_template(
+    system = SystemMessagePromptTemplate.from_template(
         """
         You are Bot Bập Bẹ, a smart AI tasked with analyzing conversation & answering questions based on provided context. Here are multiple relevent conversations betweens friends within group chats as context.
         Each conversation is between XML tag <CONVERSATION> and </CONVERSATION>
@@ -41,12 +51,6 @@ def prompt():
         Start: YYYY-MM-DD (Start of the conversation)
         End: YYYY-MM-DD (End of the conversation)
         Messages: str (Messages, each message are seperated by newline, in the form of [Sender]: [Text])
-
-        ### Provided Conversations:
-        {context}
-
-        ### Question:
-        {query}
 
         ### Answer Requirements
         - Provide detailed analysis based on evidence from conversations
@@ -58,6 +62,16 @@ def prompt():
         - Each bullet point should be continous without empty line in between
         """
     )
+    human = HumanMessagePromptTemplate.from_template(
+        """
+        ### Provided Conversations:
+        {context}
+
+        ### Prompt:
+        {query}
+        """
+    )
+    return ChatPromptTemplate.from_messages([system, human])
 
 
 async def answer(query: str, k=10, vectorstore=vectorstore, prompt=prompt, llm=llm):
@@ -73,10 +87,10 @@ async def answer(query: str, k=10, vectorstore=vectorstore, prompt=prompt, llm=l
         ]
         return "\n".join(conversations)
 
-    def format_html(text: str):
-        text = re.sub(r"\*\*(.*?)\*\*|__(.*?)__", r"<b>\1\2</b>", text)
-        text = re.sub(r"\*(.*?)\*|_(.*?)_", r"<i>\1\2</i>", text)
-        return text
+    def format_html(message: AIMessage):
+        text = re.sub(r"\*\*(.*?)\*\*|__(.*?)__", r"<b>\1\2</b>", message.content)
+        message = re.sub(r"\*(.*?)\*|_(.*?)_", r"<i>\1\2</i>", text)
+        return message
 
     retriever = vectorstore().as_retriever(
         search_type="similarity",
