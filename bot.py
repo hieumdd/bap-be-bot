c862357d644751f2654d5abe924ed72bcb010853
HM <hieumdd@gmail.com>
2025-03-31 10:00:44 +0700

    add: Add ziwei answer

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ from tenacity import AsyncRetrying, wait_fixed
 from logger import get_logger
 from config import config
 from models.message import Message, MessageRepository
+from ziwei.ziwei_graph import run_ziwei_graph
 import rag
 
 
@@ -21,7 +22,9 @@ logger = get_logger(__name__)
 
 
 async def post_init(application: Application):
-    await application.bot.set_my_commands([("query", "Query")])
+    await application.bot.set_my_commands(
+        [("query", "Query"), ("ziwei", "Ziwei Doushu")]
+    )
     logger.debug("Bot is running")
 
 
@@ -44,7 +47,7 @@ def queue_message():
     return _queue_message
 
 
-async def answer(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def rag_answer(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not update.message or not update.message.chat.id:
         return
     if not context.args:
@@ -64,6 +67,37 @@ async def answer(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 await asyncio.sleep(0.25)
 
 
+async def ziwei_answer(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not update.message or not update.message.chat.id:
+        return
+    if not context.args:
+        await update.message.reply_text("Empty Query")
+        return
+    question = " ".join(context.args)
+    for node_id, state in run_ziwei_graph(question):
+        async for attempt in AsyncRetrying(wait=wait_fixed(2), stop=4, reraise=True):
+            with attempt:
+                await update.message.reply_chat_action(ChatAction.TYPING)
+                if node_id == "handle_error":
+                    await update.message.reply_text(
+                        state["messages"][-1].content,
+                        parse_mode=ParseMode.HTML,
+                    )
+                if node_id == "generate_image":
+                    await update.message.reply_photo(
+                        state["birthchart_image"],
+                        caption="Lá số Tử Vi",
+                        parse_mode=ParseMode.HTML,
+                    )
+                if node_id.startswith("analyze"):
+                    for message in state.values():
+                        await update.message.reply_html(
+                            message[:4096],
+                            parse_mode=ParseMode.HTML,
+                        )
+        await asyncio.sleep(0.25)
+
+
 async def on_error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     error = context.error
     logger.error(error)
@@ -75,7 +109,8 @@ if __name__ == "__main__":
 
     text_message = filters.TEXT & ~filters.COMMAND
     application.add_handler(MessageHandler(text_message, queue_message()))
-    application.add_handler(CommandHandler("query", answer))
+    application.add_handler(CommandHandler("query", rag_answer))
+    application.add_handler(CommandHandler("ziwei", ziwei_answer))
     application.add_error_handler(on_error)
 
     application.run_polling()
