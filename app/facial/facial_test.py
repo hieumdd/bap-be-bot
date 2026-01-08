99ffd00c8e653bf35822097bd930ef7785d13d59
HM <hieumdd@gmail.com>
2026-01-09 00:00:24 +0700

    misc: Refactor to fixtures

--- app/facial/facial_test.py
+++ app/facial/facial_test.py
@@ -9,14 +9,17 @@ from app.utils.image import ImageBytesToB64
 
 class TestFacial:
     @pytest.fixture
+    def facial_graph_service(self, chat_model_service: ChatModelService):
+        return FacialGraphService(chat_model_service)
+
+    @pytest.fixture
     def image_url(self):
         path = Path(__file__).parent / "facial_test.png"
         with path.open("rb") as f:
             image_bytes = f.read()
             return ImageBytesToB64().dump(image_bytes)
 
-    def test_graph(self, image_url: str, chat_model_service: ChatModelService):
-        service = FacialGraphService(chat_model_service)
-        for node, state in service.run(image_url):
+    def test_graph(self, facial_graph_service: FacialGraphService, image_url: str):
+        for node, state in facial_graph_service.run(image_url):
             print(f">>> {node}")
             print(state)
